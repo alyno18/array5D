038ba3b3b962cf7b13b455d1987dcc7519ab99af
alyno18 <alybonomi18@gmail.com>
2021-12-14 09:19:51 +0100

    situazione iniziale

--- app/src/main/java/com/example/array5d/MainActivity.java
+++ app/src/main/java/com/example/array5d/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.array5d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String nazioni[] = {"Italia","Francia","Germania","Olanda"};
+    ListView ListaStati;
+    Button btnEsegui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListaStati = (ListView) findViewById(R.id.LvStati);
+        btnEsegui = (Button) findViewById(R.id.btnEsegui);
+
+        btnEsegui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hai premuto il tasto", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ArrayAdapter<String> aaStati = new ArrayAdapter<String>(this, R.layout.activity_main, nazioni);
+        ListaStati.setAdapter(aaStati);
     }
 }
